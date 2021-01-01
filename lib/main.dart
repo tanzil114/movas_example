d4273cfc9dd7836673f323f155dff10f83c52d76
Tanzeel Hassan <tanzilbaghoor@gmail.com>
2021-01-02 01:41:57 +0500

    Integrated ui with backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movas_example/movas/provider/provider.dart';
 import 'package:movas_example/movas/router/router.dart';
 import 'package:provider/provider.dart';
 
@@ -12,14 +13,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: router.initialRoute,
+        routes: router.calculateAllRoutes,
       ),
-      initialRoute: router.initialRoute,
-      routes: router.calculateAllRoutes,
     );
   }
 }
--- lib/movas/ui/pages/home/home_page.dart
+++ lib/movas/ui/pages/home/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:movas_example/movas/actions/feed_items_action.dart';
+import 'package:movas_example/movas/observables/feed_items_observable.dart';
+import 'package:movas_example/movas/ui/widgets/custom_loading_indicator.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,15 +10,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool useProdService = true;
+
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      FeedItemsA.of(context).getPosts(useProdService: useProdService);
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
       body: Center(
         child: Container(
-          child: Text('This is the body'),
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      useProdService = !useProdService;
+                    });
+                  },
+                  child: Text(useProdService
+                      ? 'Use Mock Service'
+                      : 'Use Prod Service')),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Card(
+                    child: Consumer<FeedItemsO>(
+                      builder: (context, feedItemsO, _) => feedItemsO?.items ==
+                              null
+                          ? CustomLoadingIndicator()
+                          : ListView.builder(
+                              itemBuilder: (context, index) => ListTile(
+                                  title: Text(feedItemsO.items[index].title)),
+                              itemCount: feedItemsO.items.length,
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
