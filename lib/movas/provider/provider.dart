567544c7968e4c40cdae5a163760c595c59ef486
Tanzeel Hassan <tanzilbaghoor@gmail.com>
2021-01-05 16:57:29 +0500

    fixed provider.dart

--- lib/movas/provider/provider.dart
+++ lib/movas/provider/provider.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:movas/provider/provider.dart';
 import 'package:movas_example/movas/observables/app_config_observable.dart';
 import 'package:movas_example/movas/observables/feed_items_observable.dart';
 import 'package:movas_example/movas/services/app_http_service.dart';
-import 'package:movas_example/movas/services/posts/posts_service.dart';
 import 'package:movas_example/movas/services/responses/feed_items.dart';
 import 'package:movas_example/movas/stores/app_config_store.dart';
 import 'package:movas_example/movas/stores/feed_items_store.dart';
@@ -19,11 +17,6 @@ List<SingleChildWidget> providers = [
 
 List<SingleChildWidget> streamProviders = [
   Provider<PublishSubject<FeedItemsResponse>>(
-    lazy: false,
-    dispose: (_, stream) => stream.close(),
-    create: (context) => PublishSubject(sync: true),
-  ),
-  Provider<PublishSubject<PostsService>>(
     lazy: false,
     dispose: (_, stream) => stream.close(),
     create: (context) => PublishSubject(sync: true),
